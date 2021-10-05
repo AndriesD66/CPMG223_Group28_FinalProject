5e4dd1e79ef940ff55bd5c4d39c2145ceb185ec4
AndriesD66 <andrediamond90@gmail.com>
2021-10-05 15:21:53 +0200

    Created main MDI container

--- CMPG_Group28_FinalProject/CMPG_Group28_FinalProject/Form1.Designer.cs
+++ CMPG_Group28_FinalProject/CMPG_Group28_FinalProject/Form1.Designer.cs
@@ -1,7 +1,7 @@
 ﻿
 namespace CMPG_Group28_FinalProject
 {
-    partial class Form1
+    partial class frmHome
     {
         /// <summary>
         /// Required designer variable.
@@ -29,13 +29,57 @@ namespace CMPG_Group28_FinalProject
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.Window = new System.Windows.Forms.ToolStripMenuItem();
+            this.Exit = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.Window,
+            this.Exit});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // Window
+            // 
+            this.Window.Name = "Window";
+            this.Window.Size = new System.Drawing.Size(63, 20);
+            this.Window.Text = "&Window";
+            // 
+            // Exit
+            // 
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(38, 20);
+            this.Exit.Text = "&Exit";
+            // 
+            // frmHome
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.menuStrip1);
+            this.IsMdiContainer = true;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmHome";
+            this.Text = "Home";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem Window;
+        private System.Windows.Forms.ToolStripMenuItem Exit;
     }
 }
 
--- CMPG_Group28_FinalProject/CMPG_Group28_FinalProject/Form1.cs
+++ CMPG_Group28_FinalProject/CMPG_Group28_FinalProject/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace CMPG_Group28_FinalProject
 {
-    public partial class Form1 : Form
+    public partial class frmHome : Form
     {
-        public Form1()
+        public frmHome()
         {
             InitializeComponent();
         }
