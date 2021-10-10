b2dff9b9ab97dd5dd5184f4dbb2a79adbd057894
AndriesD66 <andrediamond90@gmail.com>
2021-10-10 20:49:56 +0200

    Added function to log class attendance

--- CMPG_Group28_FinalProject/Access.cs
+++ CMPG_Group28_FinalProject/Access.cs
@@ -27,7 +27,7 @@ namespace CMPG_Group28_FinalProject
         SqlCommand comm;
   
         SqlDataReader read;
-
+        int[] attend = new int[4] { 0, 0, 0, 0 };
         int MemberID;
         int EntryID;
         int ExitID;
@@ -113,6 +113,7 @@ namespace CMPG_Group28_FinalProject
                 
                 MessageBox.Show(ae.Message.ToString(), "", btn, MessageBoxIcon.Warning);
             }
+            getAttendance();
         }
 
         private void Access_Load(object sender, EventArgs e)
@@ -120,6 +121,7 @@ namespace CMPG_Group28_FinalProject
             conn = new SqlConnection(Global.ConString);
             conn.Open();
             conn.Close();
+            this.FormClosed += new FormClosedEventHandler(this_FormClosed);
         }
 
         private void btnExit_Click(object sender, EventArgs e)
@@ -220,7 +222,63 @@ namespace CMPG_Group28_FinalProject
                 conn.Close();
             }
         }
-
+        private void getAttendance()
+        {
+            if (cmbClassType.SelectedIndex == -1)
+            {
+                MessageBox.Show("Please choose the class being attended!", "Error!!", btn, warn);
+            }
+            else if (cmbClassType.SelectedIndex == 1)
+            {
+                attend[0] += 1;
+            }
+            else if (cmbClassType.SelectedIndex == 2)
+            {
+                attend[1] += 1;
+            }
+            else if (cmbClassType.SelectedIndex == 3)
+            {
+                attend[2] += 1;
+            }
+            else if (cmbClassType.SelectedIndex == 4)
+            {
+                attend[3] += 1;
+            }
+        }
+        private void setAttendance()
+        {
+            try
+            {
+                conn.Open();
+                string YogaAtt = "Update ClassType Set Attendants = @YogaAtt Where ClassType = 'Yoga'";
+                comm = new SqlCommand(YogaAtt, conn);
+                comm.Parameters.AddWithValue("@YogaAtt", attend[0]);
+                comm.ExecuteNonQuery();
+                string PalAtt = "Update ClassType Set Attendants = @PalAtt Where ClassType = 'Palates'";
+                comm = new SqlCommand(PalAtt, conn);
+                comm.Parameters.AddWithValue("@PalAtt", attend[1]);
+                comm.ExecuteNonQuery();
+                string CrossAtt = "Update ClassType Set Attendants = @CrossAtt Where ClassType = 'CrossFit'";
+                comm = new SqlCommand(CrossAtt, conn);
+                comm.Parameters.AddWithValue("@CrossAtt", attend[2]);
+                comm.ExecuteNonQuery();
+                string SpinAtt = "Update ClassType Set Attendants = @SpinAtt Where ClassType = 'Spin'";
+                comm = new SqlCommand(SpinAtt, conn);
+                comm.Parameters.AddWithValue("@SpinAtt", attend[3]);
+                comm.ExecuteNonQuery();
+                conn.Close();
+            }
+            catch (Exception ae)
+            {
+                MessageBox.Show(ae.Message.ToString(), "Error", btn, warn);
+                conn.Close();
+            }
+            
+        }
+        private void this_FormClosed(object sender, EventArgs e)
+        {
+            setAttendance();
+        }
       
     }
 }
